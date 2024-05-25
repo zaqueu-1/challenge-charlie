f8de055f40281805ff38b43a52ec2fa490223c17
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2024-05-25 11:08:32 -0300

    feat: Add early WeatherBox component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import SearchInput from "@/components/SearchInput/search.input"
 import MainContainer from "@/components/MainContainer/main.container"
 import { useState } from 'react'
+import WeatherBox from "@/components/WeatherBox/weather.box"
 
 export default function Home() {
   const [search, setSearch] = useState('')
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <MainContainer>
       <SearchInput search={search} onSearchChange={(val) => setSearch(val)} />
+      <WeatherBox />
     </MainContainer>
   );
 }
--- src/components/SearchInput/search.input.tsx
+++ src/components/SearchInput/search.input.tsx
@@ -10,8 +10,8 @@ function SearchInput(props: SearchProps) {
     const { search, onSearchChange } = props
 
   return (
-    <div className='flex w-full max-w-[50%] items-center gap-4 h-[90px] bg-white p-2'>
-      <CompassIcon className='w-14 h-14 rotate-90' fill='rgb(var(--text))' />
+    <div className='flex w-full max-w-[50%] items-center gap-4 h-[90px] bg-white px-2 py-4'>
+      <CompassIcon className='w-16 h-16 rotate-90' fill='rgb(var(--text))' />
       <input type='text' className='w-[90%] h-full text-[32px] text-[rgb(var(--text))] font-bold placeholder:text-[rgb(var(--text))]' placeholder='Buscar cidade...' value={search} onChange={(e) => onSearchChange(e.target.value)}/>
     </div>
   )
